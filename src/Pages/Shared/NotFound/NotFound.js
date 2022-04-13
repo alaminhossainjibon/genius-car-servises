cbe9147d7b4626db43f2f8a484ed986265e2d0fe
alaminhossainjibon <mdalamin7622@gmail.com>
2022-04-13 12:52:22 +0600

    login and register form create

--- src/Pages/Shared/NotFound/NotFound.js
+++ src/Pages/Shared/NotFound/NotFound.js
@@ -2,7 +2,7 @@ import React from 'react';
 import notfound from '../../../images/notfound.gif'
 const NotFound = () => {
     return (
-        <div>
+        <div className='container w-50 mx-auto'>
             <h1 className='text-primary text-center'>Page is Error</h1>
             <img src={notfound} alt="" />
         </div>
